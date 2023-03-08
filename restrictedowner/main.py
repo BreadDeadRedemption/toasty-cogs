adf6bde4f0c3eed8bfcc3f4788c846d9e1a8c5a4
BreadDeadRedemption <117962181+BreadDeadRedemption@users.noreply.github.com>
2023-03-07 22:27:27 -0600

    Update main.py

--- restrictedowner/main.py
+++ restrictedowner/main.py
@@ -1,61 +1,66 @@
 import discord
 from redbot.core import commands
-import asyncio
 
 class RestrictedOwner(commands.Cog):
-    
+    """Manage your bot's owners and approved owners from within Discord!"""
+
     def __init__(self, bot):
         self.bot = bot
-        self.approved_owners = set()
+        self.approved_owners = {self.bot.owner_id}
 
     @commands.group(invoke_without_command=True)
     async def owner(self, ctx):
-        """Owner management commands"""
-        if ctx.invoked_subcommand is None:
-            approved = ""
-            for _id in list(self.approved_owners):
-                owner = self.bot.get_user(_id)
-                approved += f"- {owner} (`{owner.id}`)\n"
-            embed = discord.Embed(
-                title="Bot Owners and Approved Owners:",
-                description=f"**Bot Owner:**\n{self.bot.owner_id}\n**Approved Owners:**\n{approved}",
-                color=await ctx.embed_color(),
-            )
-            await ctx.send(embed=embed)
+        """View bot owners and approved owners."""
+        owners = []
+        for owner_id in self.bot.owner_ids:
+            owner = self.bot.get_user(owner_id)
+            if owner:
+                owners.append(f"{owner.name}#{owner.discriminator} (`{owner.id}`)")
+        approved = []
+        for owner_id in self.approved_owners:
+            owner = self.bot.get_user(owner_id)
+            if owner:
+                approved.append(f"{owner.name}#{owner.discriminator} (`{owner.id}`)")
+        embed = discord.Embed(
+            title="Bot Owners and Approved Owners:",
+            description=f"**Bot Owner:**\n{owners[0]}\n**Approved Owners:**\n{', '.join(approved) or 'None'}",
+            color=await ctx.embed_color(),
+        )
+        await ctx.send(embed=embed)
 
-    @owner.command(invoke_without_command=True)
-    async def add(self, ctx, *, user: discord.User):
-        """Add an approved owner. This user will be able to send restricted commands that require owner approval."""
-        user = self.bot.get_user(user.id)
-        if user.id == self.bot.owner_id:
-            return await ctx.send("That user is already the bot owner.")
+    @owner.command()
+    async def add(self, ctx, user: discord.User):
+        """Add an approved owner."""
+        if user.id in self.bot.owner_ids:
+            return await ctx.send("That user is already one of the bot owners.")
         elif user.id in self.approved_owners:
             return await ctx.send("That user is already an approved owner.")
         else:
             self.approved_owners.add(user.id)
             await ctx.tick()
-            await ctx.send(f"{user} has been added as an approved owner.")
+            await ctx.send(f"{user.mention} has been added as an approved owner.")
 
-    @commands.command(hidden=True, name="ownreq")
-    async def owner_request(self, ctx, *, command):
+    @commands.command()
+    async def ownreq(self, ctx, *, command):
         """Sends a request to the bot owner to approve a restricted command."""
         user = ctx.author
         if user.id not in self.approved_owners:
             return await ctx.send("You are not an approved owner.")
         else:
-            owner = self.bot.get_user(self.bot.owner_id)
-            msg = f"{user} has requested permission for `{command}`. Approve? (y/n)"
+            owner = self.bot.owner
+            if not owner:
+                return await ctx.send("Bot owner not found.")
+            msg = f"{user.mention} has requested permission for `{command}`. Approve? (y/n)"
             confirmation = await owner.send(msg)
             await confirmation.add_reaction("👍")
             await confirmation.add_reaction("👎")
-            print("Owner request sent.")
             try:
                 reaction, _ = await self.bot.wait_for(
                     "reaction_add",
                     timeout=60.0,
                     check=lambda r, u: (
                         r.message.id == confirmation.id
-                        and u.id == self.bot.owner_id
+                        and u.id == owner.id
                         and str(r.emoji) in ["👍", "👎"]
                     ),
                 )
