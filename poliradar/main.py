b901dd43b61277040bbc05accd5569405ee7d1ff
BreadDeadRedemption <117962181+BreadDeadRedemption@users.noreply.github.com>
2023-03-09 17:25:03 -0600

    Update main.py

--- poliradar/main.py
+++ poliradar/main.py
@@ -10,7 +10,7 @@ class RadarCog(commands.Cog):
         self.bot = bot
     
     @commands.command()
-    async def radar(self, ctx, name: str, economic: int, social: int, environmental: int, geopolitical: int):
+    async def radar(self, ctx, economic: int, social: int, environmental: int, geopolitical: int):
         # Define the dimensions and labels for the four axes
         labels = ['Economic', 'Social', 'Environmental', 'Geopolitical']
         dimensions = len(labels)
@@ -19,20 +19,16 @@ class RadarCog(commands.Cog):
         scale = np.array([100, 100, 100, 100])
 
         # Create a function to plot the four-axis radar chart
-        async def plot_radar(ctx, name, values):
+        async def plot_radar(ctx, values):
             angles = np.linspace(0, 2*np.pi, dimensions, endpoint=False)
             values = np.concatenate((values,[values[0]]))
             angles = np.concatenate((angles,[angles[0]]))
-
-            # Make sure that labels has the same length as angles
-            labels = ['Economic', 'Social', 'Environmental', 'Geopolitical']
 
             fig = plt.figure()
             ax = fig.add_subplot(111, polar=True)
             ax.plot(angles, values, 'o-', linewidth=2)
             ax.fill(angles, values, alpha=0.25)
             ax.set_thetagrids(angles * 180/np.pi, labels)
-            ax.set_title(name)
             ax.set_rlim(0, max(scale))
 
             # Save the plot to a bytes buffer
@@ -44,11 +40,9 @@ class RadarCog(commands.Cog):
             file = discord.File(buffer, filename='radar.png')
             await ctx.send(file=file)
 
-
-
         # Example usage:
         values = [economic, social, environmental, geopolitical]
-        await plot_radar(ctx, name, values)
+        await plot_radar(ctx, values)
 
 def setup(bot):
     bot.add_cog(RadarCog(bot))
