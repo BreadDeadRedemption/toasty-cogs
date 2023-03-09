25e96850fbceaf67240d868cf554907d53c827b1
BreadDeadRedemption <117962181+BreadDeadRedemption@users.noreply.github.com>
2023-03-09 17:38:05 -0600

    Update main.py

--- poliradar/main.py
+++ poliradar/main.py
@@ -3,13 +3,13 @@ from discord.ext import commands
 import numpy as np
 import matplotlib.pyplot as plt
 from io import BytesIO
-from redbot.core import commands
+from redbot.core import commands as rbc
 
 class RadarCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
-    @commands.command()
+    @rbc.command()
     async def radar(self, ctx, economic: int, social: int, environmental: int, geopolitical: int):
         # Define the dimensions and labels for the four axes
         labels = ['Economic', 'Social', 'Environmental', 'Geopolitical']
@@ -20,15 +20,15 @@ class RadarCog(commands.Cog):
 
         # Create a function to plot the four-axis radar chart
         async def plot_radar(ctx, values):
-            angles = np.linspace(0, 2*np.pi, dimensions, endpoint=False)[:-1]
+            angles = np.linspace(0, 2*np.pi, dimensions, endpoint=False)
             values = np.concatenate((values,[values[0]]))
-            angles = np.concatenate((angles,[angles[0]]))
+            angles = np.concatenate((angles,[angles[0]+0.1]))
 
             fig = plt.figure()
             ax = fig.add_subplot(111, polar=True)
             ax.plot(angles, values, 'o-', linewidth=2)
             ax.fill(angles, values, alpha=0.25)
-            ax.set_thetagrids(angles * 180/np.pi, labels)
+            ax.set_thetagrids(np.degrees(angles), labels)
             ax.set_rlim(0, max(scale))
 
             # Save the plot to a bytes buffer
@@ -40,9 +40,5 @@ class RadarCog(commands.Cog):
             file = discord.File(buffer, filename='radar.png')
             await ctx.send(file=file)
 
-        # Example usage:
-        values = [economic, social, environmental, geopolitical]
-        await plot_radar(ctx, values)
-
-def setup(bot):
-    bot.add_cog(RadarCog(bot))
+        # Call the function to plot the radar chart
+        await plot_radar(ctx, [economic, social, environmental, geopolitical])
