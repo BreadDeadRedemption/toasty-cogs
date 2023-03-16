06af7091075ad29376840d31d27da4f743cde577
BreadDeadRedemption <117962181+BreadDeadRedemption@users.noreply.github.com>
2023-03-16 11:49:42 -0500

    Update __init__.py

--- betterbumper/__init__.py
+++ betterbumper/__init__.py
@@ -1,6 +1,6 @@
 from redbot.core.utils import get_end_user_data_statement
 
-from .disboardreminder import DisboardReminder
+from .main import DisboardReminder
 
 __red_end_user_data_statement__ = get_end_user_data_statement(__file__)
 
