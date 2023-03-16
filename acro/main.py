8075c1df5897dc27cb08fc457dbf931d56f3386d
BreadDeadRedemption <117962181+BreadDeadRedemption@users.noreply.github.com>
2023-03-16 00:47:36 -0500

    Update main.py

--- acro/main.py
+++ acro/main.py
@@ -1,10 +1,9 @@
 import discord
 from discord.ext import commands
-from redbot.core import Config, checks
+from redbot.core import commands
+from redbot.core import Config, checks, bank
 from redbot.core.commands import Context
 from redbot.core.bot import Red
-from redbot.core import commands
-from redbot.core import bank
 import random
 import string
 import asyncio
@@ -27,6 +26,7 @@ class AcroGame(commands.Cog, name="AcroGame"):
             highest_votes=0,
             highest_voted_acro=""
         )
+        self.game_running = False
 
     async def red_delete_data_for_user(self, *, requester, user_id):
         await self.config.user_from_id(user_id).clear()
@@ -34,17 +34,22 @@ class AcroGame(commands.Cog, name="AcroGame"):
     @commands.group()
     async def acro(self, ctx: Context):
         pass
-    def generate_acronym(self):
+
+    def generate_acronym(self, length):
         acronym = ''
-        for _ in range(3):
+        for _ in range(length):
             letter = random.choice(string.ascii_uppercase)
             while letter in ['X', 'Z']:
                 letter = random.choice(string.ascii_uppercase)
-            acronym += letter
-        return acronym
+            acronym += letter + '.'
+        return acronym[:-1]
 
     async def start_game(self, ctx: Context):
-        acronym = self.generate_acronym()
+        if self.game_running:
+            await ctx.send("A game of Acro is already running.")
+            return None
+        self.game_running = True
+        acronym = self.generate_acronym(random.randint(3, 6))
         embed = discord.Embed(title="Acrophobia",
                               description=f"Game started. Create a sentence with the following acronym: {acronym}\nYou have 60 seconds to make a submission.",
                               color=0x42F56C)
@@ -52,10 +57,17 @@ class AcroGame(commands.Cog, name="AcroGame"):
         return acronym
 
     async def gather_submissions(self, ctx: Context, acronym: str):
+        if acronym is None:
+            return []
+
         submissions = []
+        submitted_users = set()
+
         def check(message):
+            if message.author in submitted_users:
+                return False
             words = message.content.upper().split()
-            if len(words) == len(acronym) and all([words[i][0] == acronym[i] for i in range(len(acronym))]):
+            if len(words) == len(acronym.split('.')) and all([words[i][0] == acronym.split('.')[i] for i in range(len(acronym.split('.')))]):
                 return True
             return False
 
@@ -63,140 +75,124 @@ class AcroGame(commands.Cog, name="AcroGame"):
             try:
                 submission = await self.bot.wait_for('message', timeout=60, check=check)
                 submissions.append((submission.content, submission.author))
+                submitted_users.add(submission.author)
                 await submission.delete()
             except asyncio.TimeoutError:
                 break
 
         return submissions
-    async def voting_phase(self, ctx: Context, submissions):
+    async def voting_phase(self, ctx: Context, acronym: str, submissions: list):
         if not submissions:
             embed = discord.Embed(title="Acrophobia",
-                                  description="No submissions received. The game has ended.",
-                                  color=0xFF0000)
+                                  description="No submissions received. Ending the game.",
+                                  color=0x42F56C)
             await ctx.send(embed=embed)
-            return
+            self.game_running = False
+            return None
 
-        description = "Acrophobia - Submissions closed\n"
-        acronym = submissions[0][0].split()[0][0] + submissions[0][0].split()[1][0] + submissions[0][0].split()[2][0]
-        description += f"Acronym was {acronym}\n--\n"
-        for idx, submission in enumerate(submissions, 1):
-            description += f"{idx}. {submission[0]}\n"
-
-        embed = discord.Embed(title="Acrophobia",
-                              description=description + "--\nVote by typing the number of the submission.",
+        embed = discord.Embed(title="Acrophobia - Submissions closed",
+                              description=f"Acronym was {acronym}\n--",
                               color=0x42F56C)
+
+        for i, (submission, _) in enumerate(submissions, start=1):
+            embed.add_field(name=f"{i}. {submission}", value="\u200b", inline=False)
+
+        embed.description += "--\nVote by typing a number of the submission"
         await ctx.send(embed=embed)
 
         votes = [0] * len(submissions)
+        voters = set()
 
         def check(message):
-            if message.content.isdigit():
-                num = int(message.content)
-                if 1 <= num <= len(submissions) and message.author != submissions[num - 1][1]:
-                    return True
+            if message.author in voters or not message.content.isdigit():
+                return False
+            num = int(message.content) - 1
+            if 0 <= num < len(submissions):
+                return True
             return False
 
         while True:
             try:
-                vote = await self.bot.wait_for('message', timeout=30, check=check)
-                votes[int(vote.content) - 1] += 1
-                embed = discord.Embed(title="Acrophobia",
-                                      description=f"{vote.author} cast their vote!",
-                                      color=0x42F56C)
-                await ctx.send(embed=embed)
+                vote = await self.bot.wait_for('message', timeout=60, check=check)
+                index = int(vote.content) - 1
+                if submissions[index][1] != vote.author:
+                    votes[index] += 1
+                    voters.add(vote.author)
+                    embed = discord.Embed(title="Acrophobia",
+                                          description=f"{vote.author} cast their vote!",
+                                          color=0x42F56C)
+                    await ctx.send(embed=embed)
                 await vote.delete()
             except asyncio.TimeoutError:
                 break
 
-        winner_idx = votes.index(max(votes))
-        winner = submissions[winner_idx][1]
-        winning_acro = submissions[winner_idx][0]
-        await self.config.member(winner).wins.set(await self.config.member(winner).wins() + 1)
+        return votes
 
+    async def end_game(self, ctx: Context, acronym: str, submissions: list, votes: list):
+        if not votes:
+            self.game_running = False
+            return
+
+        winner_index = votes.index(max(votes))
+        winner = submissions[winner_index][1]
         reward = await self.config.guild(ctx.guild).reward()
-        currency_name = await bank.get_currency_name(ctx.guild)
+
         await bank.deposit_credits(winner, reward)
 
+        await self.config.member(winner).wins.set(await self.config.member(winner).wins() + 1)
+        for user in ctx.guild.members:
+            if user != winner:
+                await self.config.member(user).losses.set(await self.config.member(user).losses() + 1)
+
+        if max(votes) > await self.config.member(winner).highest_votes():
+            await self.config.member(winner).highest_votes.set(max(votes))
+            await self.config.member(winner).highest_voted_acro.set(submissions[winner_index][0])
+
         embed = discord.Embed(title="Acrophobia",
-                              description=f"Winner is {winner} with {max(votes)} votes.\nThey win {reward} {currency_name}!\n{winning_acro}",
+                              description=f"Winner is {winner} with {max(votes)} votes.\nThey win {reward} {bank.get_currency_name(ctx.guild)}!\n{submissions[winner_index][0]}",
                               color=0x42F56C)
         await ctx.send(embed=embed)
+        self.game_running = False
 
     @acro.command()
-    async def play(self, ctx: Context):
+    async def start(self, ctx: Context):
         acronym = await self.start_game(ctx)
         submissions = await self.gather_submissions(ctx, acronym)
-        await self.voting_phase(ctx, submissions)
+        votes = await self.voting_phase(ctx, acronym, submissions)
+        await self.end_game(ctx, acronym, submissions, votes)
+
     @acro.command()
     async def leaderboard(self, ctx: Context):
         leaderboard = await self.config.guild(ctx.guild).leaderboard()
-        sorted_leaderboard = sorted(leaderboard.items(), key=lambda x: x[1], reverse=True)
-
-        def create_embed(page):
-            embed = discord.Embed(title="Acrophobia Leaderboard",
-                                  description=f"Top 10 players in {ctx.guild.name}",
-                                  color=0x42F56C)
-            start = (page - 1) * 10
-            end = start + 10
-            for rank, (user_id, wins) in enumerate(sorted_leaderboard[start:end], start=start + 1):
-                user = self.bot.get_user(user_id)
-                embed.add_field(name=f"{rank}. {user}", value=f"Wins: {wins}", inline=False)
-            return embed
-
-        embed = create_embed(1)
-        leaderboard_message = await ctx.send(embed=embed)
-
-        if len(sorted_leaderboard) > 10:
-            left_arrow = "⬅️"
-            right_arrow = "➡️"
-            await leaderboard_message.add_reaction(left_arrow)
-            await leaderboard_message.add_reaction(right_arrow)
-
-            def check(reaction, user):
-                return user == ctx.author and reaction.emoji in [left_arrow, right_arrow]
-
-            page = 1
-            while True:
-                try:
-                    reaction, user = await self.bot.wait_for('reaction_add', timeout=60, check=check)
-                    await reaction.remove(user)
-                    if reaction.emoji == left_arrow and page > 1:
-                        page -= 1
-                    elif reaction.emoji == right_arrow and page * 10 < len(sorted_leaderboard):
-                        page += 1
-                    embed = create_embed(page)
-                    await leaderboard_message.edit(embed=embed)
-                except asyncio.TimeoutError:
-                    break
+        sorted_leaderboard = sorted(leaderboard.items(), key=lambda x: x[1], reverse=True)[:10]
+        embed = discord.Embed(title="Acro Leaderboard", description="", color=0x42F56C)
+        for i, (user_id, wins) in enumerate(sorted_leaderboard, start=1):
+            user = self.bot.get_user(user_id)
+            if user:
+                embed.add_field(name=f"{i}. {user}", value=f"{wins} wins", inline=False)
+        await ctx.send(embed=embed)
 
     @acro.command()
     async def stats(self, ctx: Context, user: discord.Member = None):
         if not user:
             user = ctx.author
-
-        wins = await self.config.member(user).wins()
-        losses = await self.config.member(user).losses()
-        wl_ratio = wins / (wins + losses) if wins + losses > 0 else 0
-        highest_votes = await self.config.member(user).highest_votes()
-        highest_voted_acro = await self.config.member(user).highest_voted_acro()
-
-        embed = discord.Embed(title=f"Acrophobia Stats for {user}",
-                              color=0x42F56C)
-        embed.add_field(name="Wins", value=wins)
-        embed.add_field(name="Losses", value=losses)
-        embed.add_field(name="W/L Ratio", value=f"{wl_ratio:.2f}")
-        embed.add_field(name="Highest Voted Submission", value=f"{highest_voted_acro} ({highest_votes} votes)")
-
+        stats = await self.config.member(user).all()
+        embed = discord.Embed(title=f"{user}'s Acro Stats", description="", color=0x42F56C)
+        embed.add_field(name="Wins", value=stats["wins"])
+        embed.add_field(name="Losses", value=stats["losses"])
+        embed.add_field(name="W/L Ratio", value=f"{stats['wins'] / (stats['wins'] + stats['losses']):.2f}")
+        embed.add_field(name="Highest Voted Acro", value=stats["highest_voted_acro"])
+        embed.add_field(name="Highest Votes", value=stats["highest_votes"])
         await ctx.send(embed=embed)
 
     @acro.command()
-    @checks.admin()
-    async def set_reward(self, ctx: Context, value: int):
-        await self.config.guild(ctx.guild).reward.set(value)
-        embed = discord.Embed(title="Acrophobia",
-                              description=f"Reward for winning the game has been set to {value}.",
-                              color=0x42F56C)
-        await ctx.send(embed=embed)
+    @checks.admin_or_permissions(manage_guild=True)
+    async def set_reward(self, ctx: Context, reward: int):
+        if reward <= 0:
+            await ctx.send("Reward must be a positive number.")
+            return
+        await self.config.guild(ctx.guild).reward.set(reward)
+        await ctx.send(f"Reward for winning Acro is now set to {reward} {bank.get_currency_name(ctx.guild)}.")
 
 def setup(bot: Red):
     bot.add_cog(AcroGame(bot))
