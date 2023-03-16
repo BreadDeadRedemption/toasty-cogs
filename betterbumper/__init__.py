ede250f6bd8212716fdaaf5d30d7635d3a2de7db
BreadDeadRedemption <117962181+BreadDeadRedemption@users.noreply.github.com>
2023-03-16 11:49:09 -0500

    Create __init__.py

---
+++ betterbumper/__init__.py
@@ -0,0 +1,10 @@
+from redbot.core.utils import get_end_user_data_statement
+
+from .disboardreminder import DisboardReminder
+
+__red_end_user_data_statement__ = get_end_user_data_statement(__file__)
+
+
+async def setup(bot):
+    cog = DisboardReminder(bot)
+    bot.add_cog(cog)
